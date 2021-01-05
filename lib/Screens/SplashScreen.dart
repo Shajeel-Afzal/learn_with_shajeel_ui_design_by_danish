e2260f453dc52b217a8b3b17be471200b2b6641a
arsalanumar <Arxlanumer50@gmail.com>
2021-01-05 23:29:05 +0500

    Dashboard

--- lib/Screens/SplashScreen.dart
+++ lib/Screens/SplashScreen.dart
@@ -23,7 +23,7 @@ class _SplashScreenState extends State<SplashScreen> {
   void startTimer() {
     Timer(Duration(seconds: 3), () {
       Navigator.pushReplacement(
-          context, MaterialPageRoute(builder: (context) => LoginScreen()));
+          context, MaterialPageRoute(builder: (context) => LoginScreenWidget()));
     });
   }
 
